1d79ec541915ed34e8795da8e790c0e209776466
sushant bhaiswar <sb.bhaiswar@gmail.com>
2022-08-06 13:39:19 -0700

    gghdhh

---
+++ src/logger/logger.js
@@ -0,0 +1,5 @@
+let name = "Sushant Bhaiswar"
+function welcome() {
+    console.log('Welcome to my application. I am' , name ,'and a part of FunctionUp Plutonium cohort. ')
+}
+module.exports.wel_com =welcome
---
+++ src/validator/formatter.js
@@ -0,0 +1,12 @@
+function trim() {
+    console.log("   Sushant   Bhaiswar    ".trim())
+}
+function changetoLowerCase() {
+    console.log("Sushant Bhaiswar".toLowerCase())
+}
+function changetoUpperCase() {
+    console.log("Sushant Bhaiswar".toUpperCase())
+}
+module.exports.tr_im = trim
+module.exports.lo_wer = changetoLowerCase
+module.exports.up_er = changetoUpperCase
