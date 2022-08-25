446fa5f657f0c5657e34fc179d3f6b26ec23ea05
sushant bhaiswar <sb.bhaiswar@gmail.com>
2022-08-25 12:59:58 +0530

    this is updated file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
@@ -8,19 +9,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Sushant_Bhaiswar_30:WBYUu1bCYmxmZUmg@cluster0.jui41on.mongodb.net/Book-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-// app.use (
-//     function (req, res, next) {
-//         console.log ("inside GLOBAL MW");
-//         next();
-//   }
-//   );
-  
+
+
+
+    app.use(function (req, res, next) {
+        const timestamp = moment().format("YYYY-MM-DD  h:mm:ss ,");
+        const ip = "192.168.101.73 ,";
+        console.log(timestamp, ip, req.url)
+        next();
+    })
+
+
 
 app.use('/', route);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -8,10 +8,6 @@ const commonMW = require ("../middlewares/commonMiddlewares")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
-
-
-
 router.post("/createBook", BookController.createBook  )
 
 
