6945ef2641fbf7924e6ff0779b02069084fe5df7
sushant bhaiswar <sb.bhaiswar@gmail.com>
2022-08-29 16:54:55 +0530

    updated file

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -86,18 +86,10 @@ const updateUser = async function (req, res) {
 
 const postMessage = async function (req, res) {
     let message = req.body.message
-    // Check if the token is present
-    // Check if the token present is a valid token
-    // Return a different error message in both these cases
-    let token = req.headers["x-auth-token"]
-    if(!token) return res.send({status: false, msg: "token must be present in the request header"})
-    let decodedToken = jwt.verify(token, 'functionup-thorium')
 
-    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
     
-    //userId for which the request is made. In this case message to be posted.
+    
     let userToBeModified = req.params.userId
-    //userId for the logged-in user
     let userLoggedIn = decodedToken.userId
 
     //userId comparision to check if the logged-in user is requesting for their own data
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,18 @@
-const authenticate = function(req, req, next) {
+const token = require('jsonwebtoken')
+const authenticate = function (req, req, next) {
     //check the token in request header
+    const checktoken = req.hearers["x-auth-token"]
+    if (!checktoken) {
+        return res.send("Token is required")
+    }
     //validate this token
-
-    next()
+    const verifytoken = token.verify(checktoken , "functionup-thorium")
+    if (!verifytoken) res.send("token is not valid")
+        next()
 }
 
 
-const authorise = function(req, res, next) {
+const authorise = function (req, res, next) {
     // comapre the logged in user's id and the id in request
     next()
 }
